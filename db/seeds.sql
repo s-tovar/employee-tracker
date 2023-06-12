17fea40cd3bf8f3ef0ead86366898c7994dabb42
Steven Tovar-Contreras <123904059+s-tovar@users.noreply.github.com>
2023-06-11 20:35:42 -0700

    modified employee table in seeds.sql file

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,14 +5,21 @@ VALUES ("Instructor"),
        ("Intern");
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("Teacher", 70000, 1),
-       ("Librarian", 80000, 2),
-       ("Principal", 100000, 3),
-       ("Tutor", 50000, 4);
+VALUES ("AVID Teacher", 70000, 1),
+       ("Math Teacher", 80000, 2),
+       ("Librarian", 90000, 3),
+       ("Computer Librarian", 91000, 4),
+       ("Principal", 100000, 5),
+       ("Vice Principal", 99000, 6),
+       ("AVID Tutor 1", 35000, 7),
+       ("AVID Tutor 2", 35000, 8);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Instructor"),
-       ("Library"),
-       ("Administration"),
-       ("Reception"),
-       ("Intern");
+VALUES ("Marissa", "Gaustin", 1, 5),
+       ("Axel", "Molson", 2, 5),
+       ("Grace", "Dominguez", 3, 5),
+       ("Richard", "Wilson", 4, 3),
+       ("James", "Gallardo", 5),
+       ("Ray", "Espinoza", 6, 5),
+       ("Precious", "Smith", 7, 1),
+       ("Darling", "Santiago", 8, 1);
