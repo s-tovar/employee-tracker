cfbe324ae2672288e9ec31588cec725ef79f2cfd
Steven Tovar-Contreras <123904059+s-tovar@users.noreply.github.com>
2023-06-12 00:10:13 -0700

    added a main menu function that prompts the user in server.js file

--- server.js
+++ server.js
@@ -15,8 +15,52 @@ const db = mysql.createConnection(
   console.log(`Connected to the employee_tracker database.`)
 );
 
-db.connect(err => {
-    if (err) throw err;
-    console.log('Database connected.');
-    employee_tracker();
-});
+// db.connect(err => {  --> is this needed? is it already connected?
+//     if (err) throw err;
+//     console.log('Database connected.');
+//     employee_tracker();
+// });
+
+// main menu function that prompts user
+const mainMenu = () => {
+    inquirer.prompt({
+        type: 'list',
+        name: 'action',
+        message: 'What would you like to do?',
+        choices: [
+            'View all departments',
+            'View all roles',
+            'View all employees',
+            'Add a department',
+            'Add role',
+            'Add employee',
+            'Update an employee role',
+            'Exit',
+        ]
+    }).then(({ action }) => {
+        switch (action) {
+            case 'View all departments':
+                viewAllDepartments();
+                break;
+            case 'View all roles':
+                viewAllRoles();
+                break;
+            case 'View all employees':
+                viewAllEmployees();;
+                break;
+            case 'Add a deparment':
+                addDepartment();
+                break;
+            case 'Add role':
+                addRole();
+                break;
+            case 'Add employee':
+                addEmployee();
+                break;
+            case 'Update an employee role':
+                updateEmployeeRole();
+                break;
+            default:
+        }
+    });
+};
