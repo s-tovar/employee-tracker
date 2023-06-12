6addb9685fc5a539d3685eb630276e2f721702e6
Steven Tovar-Contreras <123904059+s-tovar@users.noreply.github.com>
2023-06-11 20:46:21 -0700

    modified server.js file by importing inquirer and mysql2, and created connection to employee_tracker database

---
+++ server.js
@@ -0,0 +1,16 @@
+const inquirer = require('inquirer');
+// Import and require mysql2
+const mysql = require('mysql2');
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // TODO: Add MySQL password here
+    password: 'Password123!',
+    database: 'employee_tracker'
+  },
+  console.log(`Connected to the employee_tracker database.`)
+);
